96be384b845c570a15b032628278dac055ac913c
loren <loren@tin.cat>
2020-05-10 21:10:49 +0200

    WIP

--- classes/User.class.php
+++ classes/User.class.php
@@ -2,6 +2,15 @@
 
 namespace CherrycakeApp;
 
-class User extends \Cherrycake\Item implements \Cherrycake\LoginUser {
+class User extends \Cherrycake\LoginUser {
+    protected $tableName = "users";
+    protected $userNameFieldName = "email";
+	protected $encryptedPasswordFieldName = "passwordHash";
     
+    protected $fields = [
+        "id" => ["type" => \Cherrycake\DATABASE_FIELD_TYPE_INTEGER],
+        "name" => ["type" => \Cherrycake\DATABASE_FIELD_TYPE_STRING],
+        "email" => ["type" => \Cherrycake\DATABASE_FIELD_TYPE_STRING],
+        "passwordHash" => ["type" => \Cherrycake\DATABASE_FIELD_TYPE_STRING]
+    ];
 }
